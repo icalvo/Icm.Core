9a088cdc4fd099e70c4534d820e0873836802e34
Ignacio Calvo <ignacio.calvo@gmail.com>
2012-08-05 19:12:40 +0200

    Move BaseSortedCollectionTest.vb under StructKeyClassValue dir

--- Src/Icm.Core.Tests/Collections/SortedCollection/StructKeyClassValue/BaseSortedCollectionTest.vb
+++ Src/Icm.Core.Tests/Collections/SortedCollection/StructKeyClassValue/BaseSortedCollectionTest.vb
@@ -43,6 +43,7 @@ Public Class BaseSortedCollectionTest
         coll.Add(14, "value2")
         Dim actual = coll.GetFreeKey(13)
 
+        ' Assert
         Assert.IsTrue(actual.HasValue)
         Assert.AreEqual(expected, actual)
     End Sub
@@ -62,25 +63,58 @@ Public Class BaseSortedCollectionTest
     <Test>
     Public Sub ToString_EmptyCollection()
         Dim coll = GetNewCollection()
-        Dim expected As String = ""
+        Dim expected As String =
+            "NC 12 ---" & vbCrLf &
+            "NC 14 ---" & vbCrLf
 
         ' Act
-        'coll.Add(Long.MaxValue, "value1")
+        Dim actual = coll.ToString(12, 14)
+
+        ' Assert
+        Assert.AreEqual(expected, actual)
+    End Sub
+
+    <Test()>
+    Public Sub ToString_OneElementAtStart_AppearsAtStart()
+        Dim coll = GetNewCollection()
+        Dim expected As String =
+            "-> 12 value1" & vbCrLf &
+            "NC 14 ---" & vbCrLf
+
+        ' Act
+        coll.Add(12, "value1")
         Dim actual = coll.ToString(12, 14)
 
         Assert.AreEqual(expected, actual)
     End Sub
 
-    <Test>
-    Public Sub ToString_OneElement()
+    <Test()>
+    Public Sub ToString_OneElementInTheMiddle_AppearsInTheMiddle()
         Dim coll = GetNewCollection()
-        Dim expected As String = Nothing
+        Dim expected As String =
+            "NC 12 ---" & vbCrLf &
+            "-> 13 value1" & vbCrLf &
+            "NC 14 ---" & vbCrLf
 
         ' Act
-        'coll.Add(Long.MaxValue, "value1")
-        Dim actual = coll.ToString
+        coll.Add(13, "value1")
+        Dim actual = coll.ToString(12, 14)
 
         Assert.AreEqual(expected, actual)
     End Sub
 
+
+    <Test()>
+    Public Sub ToString_OneElementInTheEnd_AppearsInTheEnd()
+        Dim coll = GetNewCollection()
+        Dim expected As String =
+            "NC 12 ---" & vbCrLf &
+            "-> 14 value1" & vbCrLf
+
+        ' Act
+        coll.Add(14, "value1")
+        Dim actual = coll.ToString(12, 14)
+
+        Assert.AreEqual(expected, actual)
+    End Sub
 End Class
