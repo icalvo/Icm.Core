2546f146f90a0c4735b95b053fdaf8a8f56f7d94
Ignacio Calvo <ignacio.calvo@gmail.com>
2012-08-27 19:50:38 +0200

    CompiledFunction test enhancements

--- Src/Icm.Core.Tests/Compilation/CompiledFunctionTest.vb
+++ Src/Icm.Core.Tests/Compilation/CompiledFunctionTest.vb
@@ -4,46 +4,57 @@ Imports Icm.Compilation
 <TestFixture(), Category("Icm")>
 Public Class CompiledFunctionTest
 
-    Private ReadOnly Namespaces As New List(Of String)()
-
-    <Test()>
-    Public Sub CompileOkTest()
+    <TestCase("Nothing", 1, 1, Result:=0)>
+    <TestCase("Nothing", 2, 5, Result:=0)>
+    <TestCase("x+y", 1, 1, Result:=2)>
+    <TestCase("x+y", 2, 5, Result:=7)>
+    <TestCase("x*y", 1, 1, Result:=1)>
+    <TestCase("x*y", 2, 5, Result:=10)>
+    <TestCase("x..y", 1, 1, ExpectedException:=GetType(CompileException))>
+    Public Function Evaluate_Test(code As String, param1 As Integer, param2 As Integer) As Integer
         Dim Funct As CompiledFunction(Of Integer)
         Funct = New VBCompiledFunction(Of Integer)
         Funct.AddParameter(Of Integer)("x")
         Funct.AddParameter(Of Integer)("y")
-        Funct.Code = "x+y"
+        Funct.Code = code
         Funct.CompileAsExpression()
         Assert.That(Funct.CompilerErrors.Count = 0)
-    End Sub
 
-    <Test()>
-    Public Sub EvaluateTest()
+        Return Funct.Evaluate(param1, param2)
+    End Function
+
+End Class
+
+<TestFixture(), Category("Icm")>
+Public Class VBCompiledFunctionTest
+
+    <Test>
+    Public Sub GeneratedCode_Test()
         Dim Funct As CompiledFunction(Of Integer)
         Funct = New VBCompiledFunction(Of Integer)
         Funct.AddParameter(Of Integer)("x")
         Funct.AddParameter(Of Integer)("y")
-        Funct.Code = "x+y"
-        Funct.CompileAsExpression()
-        Assert.AreEqual(10, Funct.Evaluate(5, 5))
+        Funct.Code = "x+y*y"
+
+        Dim expected = _
+            "Imports System" & vbCrLf & _
+            "Imports System.Xml" & vbCrLf & _
+            "Imports System.Data" & vbCrLf & _
+            "Imports System.Linq" & vbCrLf & _
+            "Imports Microsoft.VisualBasic" & vbCrLf & _
+            "Namespace dValuate" & vbCrLf & _
+            " Class EvalRunTime " & vbCrLf & _
+            "  Public Function EvaluateIt( _" & vbCrLf & _
+            "   ByVal x As Int32, _" & vbCrLf & _
+            "   ByVal y As Int32 _" & vbCrLf & _
+            "  ) As Int32" & vbCrLf & _
+            "    Return x+y*y" & vbCrLf & _
+            "  End Function" & vbCrLf & _
+            " End Class " & vbCrLf & _
+            "End Namespace" & vbCrLf
+
+        Assert.That(Funct.GeneratedCode, [Is].EqualTo(expected))
     End Sub
 
-    <Test()>
-    Public Sub CompileSyntaxErrorTest()
-
-        Dim Funct As New VBCompiledFunction(Of Integer)
-        Funct.AddParameter(Of Integer)("x")
-        Funct.AddParameter(Of Integer)("y")
-        Funct.Code = "x..y"
-
-        Assert.Throws(Of CompileException)(Sub()
-                                               Funct.CompileAsExpression()
-                                           End Sub)
-        Dim list = Funct.CompilerErrors
-        Dim errores = list.Where(Function(ce) Not ce.IsWarning)
-        Dim warnings = list.Where(Function(ce) ce.IsWarning)
-        Assert.That(errores.Count = 1)
-        Assert.That(warnings.Count = 0)
-    End Sub
 
 End Class
