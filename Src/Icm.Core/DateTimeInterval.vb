4dbe0f7889f0cd31c091342fcfe5993d3033fd5a
icalvo <icalvo@id-61>
2012-10-03 16:40:48 +0200

    Replace Debug.Assert with exception throwing

--- Src/Icm.Core/DateTimeInterval.vb
+++ Src/Icm.Core/DateTimeInterval.vb
@@ -1,4 +1,6 @@
-﻿Namespace Icm
+﻿Imports Icm.Localization
+
+Namespace Icm
 
     ''' <summary>
     ''' Defines a non-negative interval between two given dates.
@@ -61,7 +63,8 @@
         End Sub
 
         Public Sub SetDuration(ByVal newDuration As TimeSpan)
-            Debug.Assert(newDuration >= TimeSpan.Zero)
+            If newDuration < TimeSpan.Zero Then Throw New ArgumentOutOfRangeException("Duration must not be negative")
+
             SetInterval(start_, start_.Add(newDuration))
         End Sub
 
@@ -73,14 +76,15 @@
         ''' <param name="endDate"></param>
         ''' <remarks></remarks>
         Public Sub SetInterval(ByVal startDate As Date, ByVal endDate As Date)
-            Debug.Assert(startDate <= endDate)
-            Debug.Assert(startDate < Date.MaxValue)
+            If startDate > endDate Then Throw New ArgumentException("Start date must be less or equal than end date")
+            If startDate = Date.MaxValue Then Throw New ArgumentOutOfRangeException("Start date must not be Date.MaxValue")
+
             start_ = startDate
             end_ = endDate
         End Sub
 
         Public Sub SetInterval(ByVal startDate As Date, ByVal dur As TimeSpan)
-            Debug.Assert(dur >= TimeSpan.Zero)
+            If dur < TimeSpan.Zero Then Throw New ArgumentOutOfRangeException("Duration must not be negative")
             SetInterval(startDate, startDate.Add(dur))
         End Sub
 
@@ -88,68 +92,18 @@
             SetInterval(pointDate, pointDate)
         End Sub
 
-        Public Overridable Function Description() As String
+        Public Overridable Function Description() As Translation
             If [End] = Date.MaxValue Then
-                Return String.Format("from {0:dd/MM/yyyy HH:mm:ss}, indefinitely", Start)
+                Return TransF("from {0:dd/MM/yyyy HH:mm:ss}, indefinitely", Start)
             ElseIf [End] = Date.MinValue Then
-                Return String.Format("{0:dd/MM/yyyy HH:mm:ss}", Start)
+                Return TransF("{0:dd/MM/yyyy HH:mm:ss}", Start)
             Else
-                Return String.Format("between {0:dd/MM/yyyy HH:mm:ss} and {1:dd/MM/yyyy HH:mm:ss} ({2})", Start, [End], Duration.ToAbbrev)
+                Return TransF("between {0:dd/MM/yyyy HH:mm:ss} and {1:dd/MM/yyyy HH:mm:ss} ({2})", Start, [End], Duration.ToAbbrev)
             End If
         End Function
 
         Public Function Contains(ByVal aDate As Date) As Boolean
             Return Start <= aDate AndAlso aDate < [End]
-        End Function
-
-        ' -------------
-
-        ReadOnly Property Duración() As TimeSpan
-            Get
-                Return [End].Subtract(Start)
-            End Get
-        End Property
-
-        Public Sub EmpezarEn(ByVal newStart As Date)
-            StartWith(newStart)
-        End Sub
-
-        Public Sub Establecer(ByVal newStart As Date)
-            StartWith(newStart)
-        End Sub
-        Public Sub Desplazar(ByVal offsetSpan As TimeSpan)
-            Offset(offsetSpan)
-        End Sub
-
-        Public Sub CambiarTamaño(ByVal newDuration As TimeSpan)
-            SetDuration(newDuration)
-        End Sub
-
-        ''' <summary>
-        ''' This is the only method that actually modifies the values of the interval.
-        ''' Every other modification method use it, directly or indirectly.
-        ''' </summary>
-        ''' <param name="startDate"></param>
-        ''' <param name="endDate"></param>
-        ''' <remarks></remarks>
-        Public Sub Establecer(ByVal startDate As Date, ByVal endDate As Date)
-            Debug.Assert(startDate <= endDate)
-            Debug.Assert(startDate < Date.MaxValue)
-            start_ = startDate
-            end_ = endDate
-        End Sub
-
-        Public Sub Establecer(ByVal startDate As Date, ByVal dur As TimeSpan)
-            Debug.Assert(dur >= TimeSpan.Zero)
-            SetInterval(startDate, startDate.Add(dur))
-        End Sub
-
-        Public Overridable Function Descripción() As String
-            Return Description()
-        End Function
-
-        Public Function Contiene(ByVal aDate As Date) As Boolean
-            Return Contains(aDate)
         End Function
 
     End Class
