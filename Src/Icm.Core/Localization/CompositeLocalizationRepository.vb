d7a7283256fb5a06130ae54b9ca4c3d230c8d5f5
Ignacio Calvo <ignacio.calvo@gmail.com>
2014-01-15 00:57:27 +0100

    CompositeLocalizationRepository

---
+++ Src/Icm.Core/Localization/CompositeLocalizationRepository.vb
@@ -0,0 +1,47 @@
+﻿Namespace Icm.Localization
+
+    Public Class CompositeLocalizationRepository
+        Implements ILocalizationRepository
+
+        Private ReadOnly _subrepositories As IEnumerable(Of ILocalizationRepository)
+
+        Public Sub New(subrepositories As IEnumerable(Of ILocalizationRepository))
+            If subrepositories Is Nothing Then
+                Throw New ArgumentNullException("subrepositories")
+            End If
+
+            If subrepositories.Count = 0 Then
+                Throw New ArgumentException("subrepositories")
+            End If
+
+            _subrepositories = subrepositories.ToList()
+        End Sub
+
+        Public Sub New(ParamArray subrepositories() As ILocalizationRepository)
+            If subrepositories Is Nothing Then
+                Throw New ArgumentNullException("subrepositories")
+            End If
+
+            If subrepositories.Count = 0 Then
+                Throw New ArgumentException("subrepositories")
+            End If
+
+            _subrepositories = subrepositories.ToList()
+        End Sub
+
+        Default Public ReadOnly Property ItemForCulture(lcid As Integer, key As String) As String Implements ILocalizationRepository.ItemForCulture
+            Get
+                For Each subrepository In _subrepositories
+                    Dim result As String = subrepository.ItemForCulture(lcid, key)
+                    If result IsNot Nothing Then
+                        Return result
+                    End If
+                Next
+
+                Return Nothing
+            End Get
+        End Property
+    End Class
+
+End Namespace
+
